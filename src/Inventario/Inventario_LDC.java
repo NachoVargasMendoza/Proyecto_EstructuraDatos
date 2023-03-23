edd71595ae8a870ccfa6d0b9edb6e3ca741f6539
MijaDs <rmhborsig@gmail.com>
2023-03-23 15:17:00 -0600

    Mija
    Modificaciones varias

--- src/Inventario/Inventario_LDC.java
+++ src/Inventario/Inventario_LDC.java
@@ -4,10 +4,80 @@
  */
 package Inventario;
 
+import Proyecto_EstructuraDeDatos.Articulos;
+
 /**
  *
  * @author Ignac
  */
-public class Inventario_LDC {
+public class Inventario_LDC extends Articulos {
+
+    private Nodo_LDC inicio;
+    private Nodo_LDC fin;
+
+    public Inventario_LDC(String nombre, String descripcion, double precio, int cantidad) {
+        super(nombre, descripcion, precio, cantidad);
+        this.inicio = null;
+        this.fin = null;
+    }
+
+    public boolean vacio() {
+        if (inicio == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void agregar(String nombre, String descripcion,double  precio, int cantidad) {
+
+        Articulos articuloNuevo = new Articulos(nombre, descripcion, 0, 0);
+
+        articuloNuevo.setNombre(nombre);
+        articuloNuevo.setDescripcion(descripcion);
+        articuloNuevo.setPrecio(precio);
+        articuloNuevo.setCantidad(cantidad);
+
+        Nodo_LDC nuevo = new Nodo_LDC();
+        nuevo.setDatos(articuloNuevo);
+
+        if (vacio()) {
+            inicio = nuevo;
+            
+            fin = nuevo;
+            fin.setSiguiente(inicio);
+            inicio.setAnterior(fin);
+        } else if(articuloNuevo.getPrecio()<inicio.getDatos().getPrecio()){
+            nuevo.setSiguiente(inicio);
+            inicio=nuevo;
+            fin.setSiguiente(inicio);
+            inicio.setAnterior(fin);
+        }else if(articuloNuevo.getPrecio()>fin.getDatos().getPrecio()){
+            fin.setSiguiente(inicio);
+            fin=fin.getSiguiente();
+            fin.setSiguiente(inicio);
+            inicio.setAnterior(fin);
+        }else{
+            Nodo_LDC aux = new Nodo_LDC();
+            while(aux.getSiguiente().getDatos().getPrecio()<articuloNuevo.getPrecio()){
+                aux=aux.getSiguiente();
+            }
+            nuevo.setSiguiente(aux.getSiguiente());
+            nuevo.setAnterior(aux);
+            aux.setSiguiente(nuevo);
+            nuevo.getSiguiente().setAnterior(nuevo);
+        }
+    }
     
+    public String toString(){
+        String s ="";
+        if(!vacio()){
+            Nodo_LDC aux = inicio;
+            while (aux!=null) {                
+                s+=aux.getDatos().getNombre()+aux.getDatos().getDescripcion()+aux.getDatos().getPrecio()+aux.getDatos().getCantidad();
+            }
+        }
+        return s;
+    }
+
 }
